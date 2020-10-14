2b297f80af84b3f97cdb82a22647b49032dbc56f
Metails <dengjianhua2397462613#gmail.com>
2020-10-14 14:48:15 -0400

    replaced the original Shapes.java with the one I already had

--- Shapes.java
+++ Shapes.java
@@ -1,26 +1,34 @@
-import java.util.*;
-
-public class Shapes{
+import java.lang.Math;
+class AreaCalculator{
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the side length: ");
-    double side = input.nextDouble();
-    square(side);
-  }
+      squareAreaCalculator(6);
+      System.out.println();
+      circleAreaCalculator(6);
+      System.out.println();
+      triangleAreaCalculator(16,15);
+      System.out.println();
+      heronsFormula(9,11,16);
+      }//end main class
 
-  /*
-   * N: square
-   * P: to calculate the area of any square
-   * I: a side measure (double)
-   * R:
-   */
-  public static void square(double s){
-    //side times side
-    double a = s * s;
+      public static void squareAreaCalculator(double length){
+        double areaSquare = length * length;
+        System.out.print("This is the area of a Square: " +areaSquare);
+      }//end square area calculator method
 
-    //generate some output
-    System.out.println("A square with side " + s + " has an area of " + a);
-  }//close square method
-  //hi
+      public static void circleAreaCalculator(double radius){
+        double areaCircle = Math.PI * radius * radius;
+        System.out.print("This is the area of a circle: " +areaCircle);
+      }//end circle area calculator method
 
-}
+      public static void triangleAreaCalculator(double base,double height){
+       double areaTriangle = base * height / 2;
+       System.out.print("This is the area of a triangle: " + areaTriangle);
+      }//end area triangle calculator method
+
+      public static void heronsFormula(double length1, double length2, double length3){
+        double s = (length1+length2+length3)/2;
+        double area = Math.sqrt(s*(s-length1)*(s-length2)*(s-length3));
+        System.out.println("The area of triangle using the heron's formula is: " + area);
+      }//end the herone's method
+
+}//end class
