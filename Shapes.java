05195a00bec9656c2a497e9f6c30cb732926661e
Metails <dengjianhua2397462613#gmail.com>
2020-10-16 13:06:47 -0400

    Finished doing part 2 and added return

--- Shapes.java
+++ Shapes.java
@@ -1,13 +1,14 @@
 import java.lang.Math;
 class AreaCalculator{
   public static void main(String[] args) {
-      System.out.print("The area of this square is: " + squareAreaCalculator(6));
-      System.out.println();
-      System.out.print("The area of this circle is: " + circleAreaCalculator(6));
-      System.out.println();
-      System.out.print("The area of this triangle is: " + triangleAreaCalculator(16,15));
-      System.out.println();
-      System.out.print("The area of triangle using the heron's formula is: " + heronsFormula(9,11,16));
+      System.out.println("\nThe area of this square is: " + squareAreaCalculator(6));
+      System.out.println("\nThe area of this circle is: " + circleAreaCalculator(6));
+      System.out.println("\nThe area of this triangle is: " + triangleAreaCalculator(16,15));
+      System.out.println("\nThe area of triangle using the heron's formula is: " + heronsFormula(9,11,16));
+      System.out.println("\nThe volume of this cube is: " + cubeVolume(6));
+      System.out.println("\nThe volume of this triangular prism is: " + triangularPrismVolume(6,8,25));
+      System.out.println("\nThe volume of this rectangular prism is: " + rectangularPrismVolume(9,15,24));
+      System.out.println("\nThe volume of this sphere is: " + sphereVolume(8));
       }//end main class
 
       public static double squareAreaCalculator(double length){
@@ -31,4 +32,23 @@ class AreaCalculator{
         return area;
       }//end the herone's method
 
+      public static double cubeVolume(double length){
+        double volume = Math.pow(length,3);
+        return volume;
+      }//end cube method
+
+      public static double triangularPrismVolume(double base, double altitude, double height){
+        double volume = 0.5 * base * altitude * height;
+        return volume;
+      }//end triangular prism volume method
+
+      public static double rectangularPrismVolume(double length, double height, double width){
+        double volume = length * width * height;
+        return volume;
+      }//end rectangularprismvolume
+
+      public static double sphereVolume(double radius){
+        double volume = 4/3 * Math.sqrt(Math.pow(radius, 3));
+        return volume;
+      }//end sphervolume method
 }//end class
