8c01a5242e234b642c38d659d518c891d5ebbe56
Metails <dengjianhua2397462613#gmail.com>
2020-10-16 09:11:26 -0400

    Changed the code so that it's able to return the result

--- Shapes.java
+++ Shapes.java
@@ -1,34 +1,34 @@
 import java.lang.Math;
 class AreaCalculator{
   public static void main(String[] args) {
-      squareAreaCalculator(6);
+      System.out.print("The area of this square is: " + squareAreaCalculator(6));
       System.out.println();
-      circleAreaCalculator(6);
+      System.out.print("The area of this circle is: " + circleAreaCalculator(6));
       System.out.println();
-      triangleAreaCalculator(16,15);
+      System.out.print("The area of this triangle is: " + triangleAreaCalculator(16,15));
       System.out.println();
-      heronsFormula(9,11,16);
+      System.out.print("The area of triangle using the heron's formula is: " + heronsFormula(9,11,16));
       }//end main class
 
-      public static void squareAreaCalculator(double length){
+      public static double squareAreaCalculator(double length){
         double areaSquare = length * length;
-        System.out.print("This is the area of a Square: " +areaSquare);
+        return areaSquare;
       }//end square area calculator method
 
-      public static void circleAreaCalculator(double radius){
+      public static double circleAreaCalculator(double radius){
         double areaCircle = Math.PI * radius * radius;
-        System.out.print("This is the area of a circle: " +areaCircle);
+        return areaCircle;
       }//end circle area calculator method
 
-      public static void triangleAreaCalculator(double base,double height){
+      public static double triangleAreaCalculator(double base,double height){
        double areaTriangle = base * height / 2;
-       System.out.print("This is the area of a triangle: " + areaTriangle);
+       return areaTriangle;
       }//end area triangle calculator method
 
-      public static void heronsFormula(double length1, double length2, double length3){
+      public static double heronsFormula(double length1, double length2, double length3){
         double s = (length1+length2+length3)/2;
         double area = Math.sqrt(s*(s-length1)*(s-length2)*(s-length3));
-        System.out.println("The area of triangle using the heron's formula is: " + area);
+        return area;
       }//end the herone's method
 
 }//end class
